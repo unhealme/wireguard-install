359fbdc9b4707c1cfd8c7660eaa9e37fef58e6f9
Mickel Edward <o2tongpethuk@gmail.com>
2023-04-29 01:29:31 +0700

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -368,7 +368,7 @@ AllowedIPs = ${ALLOWED_IPS}" >"${HOME_DIR}/${SERVER_WG_NIC}-client-${CLIENT_NAME
 [Peer]
 PublicKey = ${CLIENT_PUB_KEY}
 PresharedKey = ${CLIENT_PRE_SHARED_KEY}
-AllowedIPs = ${CLIENT_WG_IPV4}/24,${CLIENT_WG_IPV6}/64" >>"/etc/wireguard/${SERVER_WG_NIC}.conf"
+AllowedIPs = ${CLIENT_WG_IPV4}/32,${CLIENT_WG_IPV6}/128" >>"/etc/wireguard/${SERVER_WG_NIC}.conf"
 
     wg syncconf "${SERVER_WG_NIC}" <(wg-quick strip "${SERVER_WG_NIC}")
 
